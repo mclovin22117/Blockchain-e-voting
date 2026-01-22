de5b4fc66e37338bc26e1e6326dc334814300138
reetik-rana <reetikrana203@gmail.com>
2026-01-22 14:08:09 +0530

    chore: update Web3 imports for v4

--- frontend/src/components/Admin.jsx
+++ frontend/src/components/Admin.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import Web3 from 'web3'
+import { Web3 } from 'web3'
 import { TransactionStatus } from './LoadingSpinner'
 import config from '../config'
 
